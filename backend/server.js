ef2ae4f1d90330e4894b7fe894e807a046a82d18
Anandhan <anandviswanathan28@gmail.com>
2026-07-08 08:29:19 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,25 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
+const connectDB = require("./config/db");
 const studentRoutes = require("./routes/studentRoutes");
 
 const app = express();
 
+// Connect Database
+connectDB();
+
+// Middleware
 app.use(cors());
+
+//parse incoming JSON request bodies. Else req.body would be undefined.
 app.use(express.json());
 
+// Routes
 app.use("/students", studentRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
-
+// Start Server
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
